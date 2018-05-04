9eccd766225afa64a286a9f95ad1fe5460dd5e42
Lucialiuxi <1435554190@qq.com>
2018-05-04 14:00:06 +0800

    修改了入口文件的内容

--- redux encapsulate/src/index.js
+++ redux encapsulate/src/index.js
@@ -5,8 +5,8 @@ import App from './app'
 
 import { reducerColor } from './redux+react/reducers/reducer';
 
-import {createStore} from 'redux';
-import { Provider } from 'react-redux'
+import { createStore } from './redux-learn/redux-like';
+import { Provider } from './redux-learn/connect';
 
 let store = createStore(reducerColor, {
   color: 'blue'
